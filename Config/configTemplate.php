9b154fe3c6292a53dbc0bb867482f2fc8f522af1
Pehnny <theo.piron.pro@gmail.com>
2025-01-24 13:53:26 +0100

    Refactor(Config with class)

---
+++ Config/configTemplate.php
@@ -0,0 +1,12 @@
+<?php
+
+class Config {
+    const HOST = '';
+    const PORT = '';
+    const DBNAME = '';
+    const USERNAME = '';
+    const PASSWORD = '';
+    const CHARSET = '';
+}
+
+?>
--- Controller/DatabaseController.php
+++ Controller/DatabaseController.php
@@ -1,12 +1,13 @@
 <?php
 namespace Controller;
 
-
+use Config;
 use Exception;
 use PDO;
 use PDOException;
-global $config;
-$config = include_once 'Config/config.php';
+// global $config;
+// $config = include_once 'Config/config.php';
+require 'Config/config.php';
 
 class DatabaseController
 {
@@ -16,16 +17,14 @@ class DatabaseController
     {
         global $config; // Éviter si possible !
 
-        if (!isset($config['database'])) {
+        if (!class_exists('Config')) {
             throw new Exception('Configuration de base de données manquante.');
         }
 
-        $databaseConfig = $config['database'];
-
-        $PDO_Link = "mysql:host={$databaseConfig['host']};dbname={$databaseConfig['dbname']};charset={$databaseConfig['charset']}";
+        $PDO_Link = 'mysql:host='.Config::HOST.';dbname='.Config::DBNAME.';charset='.Config::CHARSET;
 
         try {
-            $this->pdo = new PDO($PDO_Link, $databaseConfig['username'], $databaseConfig['password'], [
+            $this->pdo = new PDO($PDO_Link, Config::USERNAME, Config::PASSWORD, [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
