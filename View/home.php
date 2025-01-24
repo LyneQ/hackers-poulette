79dcae80dbb3b05e353e01fc0c8f7280fadf694e
Pehnny <theo.piron.pro@gmail.com>
2025-01-24 15:12:47 +0100

    Add(form)

--- View/home.php
+++ View/home.php
@@ -1,3 +1,25 @@
 <form action="" method="post">
-    
+    <div>
+        <label for="name">name</label>
+        <input type="text" name="name" id="name" placeholder="your name..." required/>
+    </div>
+    <div>
+        <label for="firstname">firstname</label>
+        <input type="text" name="firstname" id="firstname" placeholder="your firstname..." required/>
+    </div>
+    <div>
+        <label for="email">email</label>
+        <input type="text" name="email" id="email" placeholder="your email..." required/>
+    </div>
+    <div>
+        <label for="file">file</label>
+        <input type="file" name="file" id="file" accept="image/png, image/jpeg, image/jpg, image/gif"/>
+        <input type="text" name="file" id="file"/>
+    </div>
+    <div>
+        <label for="description">description</label>
+        <textarea name="description" id="description" placeholder="kepwouick..." required></textarea>
+    </div>
 </form>
+
+<script type="module" src="script.js" defer></script>
