34228426137cef3f99b7743e948b9b4fba75edc1
Pehnny <theo.piron.pro@gmail.com>
2025-01-24 13:52:54 +0100

    Refactor(routes with enum)

---
+++ Route.php
@@ -0,0 +1,10 @@
+<?php
+    enum Route: string {
+        case Home = "View/home.php";
+        case NotFound = "View/404.php";
+    }
+
+    enum Page: string {
+        case Home = "home";
+    }
+?>
--- index.php
+++ index.php
@@ -1,19 +1,15 @@
 <?php
 
 use Controller\DatabaseController;
-// import all necessary element
+// import all necessary elements
 require 'Controller/DatabaseController.php';
+require 'Route.php';
 
 // setup usefull variable
 $db = new DatabaseController();
-$page = $_GET['page'] ?? 'null';
+$page = $_GET['page'] ?? Page::Home;
 
-
-switch ($page) {
-    case 'home':
-        include 'View/home.php';
-        break;
-    default:
-        include 'View/404.php';
-        break;
-}
+include match ($page) {
+    Page::Home => Route::Home->value,
+    default => Route::NotFound->value,
+};
