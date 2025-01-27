0642068e6b95d60aa7b94c07144e678546580e12
Pehnny <theo.piron.pro@gmail.com>
2025-01-27 17:05:20 +0100

    Add(message to all cases)

--- utils/FileStatus.php
+++ utils/FileStatus.php
@@ -1,9 +1,8 @@
 <?php
-    enum FileStatus {
-        case FormatNotAllowed;
-        case RequestError;
-        case MaxSizeExceed;
-        case UploadFailed;
-        case OK;
+    enum FileStatus: string {
+        case FormatNotAllowed = 'File format forbidden !';
+        case MaxSizeExceeded = 'Maximum file size exceeded ! Please chose a file below 10 Mb.';
+        case UploadFailed = 'Upload failed !';
+        case OK = "ok";
     }
 ?>
