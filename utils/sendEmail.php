168522d7cd1375d11f20de67050cf5cc2a8d6ee6
Pehnny <theo.piron.pro@gmail.com>
2025-01-28 12:46:33 +0100

    Change(Exception message)

--- utils/sendEmail.php
+++ utils/sendEmail.php
@@ -33,7 +33,7 @@
 
             $mail->send();
         } catch(Exception $exception) {
-            throw new Exception($exception->getMessage());
+            throw new Exception('Mail to support was not sent ! '. $exception->getMessage());
         }
     }
 ?>
