c9b65bbf05f598acffa8c0fd16efaed1ce3d287f
Pehnny <theo.piron.pro@gmail.com>
2025-01-28 12:44:31 +0100

    Fix(indentation)

--- Config/configTemplate.php
+++ Config/configTemplate.php
@@ -1,13 +1,11 @@
 <?php
-
-class Config {
-    const HOST = '';
-    const PORT = '';
-    const DBNAME = '';
-    const USERNAME = '';
-    const PASSWORD = '';
-    const CHARSET = '';
-    const CAPTCHA_SECRET = ''; // Server side reCaptcha key
-}
-
+    class Config {
+        const HOST = '';
+        const PORT = '';
+        const DBNAME = '';
+        const USERNAME = '';
+        const PASSWORD = '';
+        const CHARSET = '';
+        const CAPTCHA_SECRET = ''; // Server side reCaptcha key
+    }
 ?>
